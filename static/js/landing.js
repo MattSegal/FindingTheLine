dae833d3ebddc703a11a44fd4d5a33ee6324589f
MattSegal <mattdsegal@gmail.com>
2016-02-22 22:44:13 +1100

    all of title positioning now js

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,19 +1,30 @@
 
+// cache the DOM
 var header = document.getElementsByTagName('header')[0]
 var body = document.getElementsByTagName('body')[0]
 var contactLink = document.getElementsByClassName('more-info')[0] 
-var styles, height, width, aspectRatio, vertCrop, bottomCrop, trueHeight, boxHeight, boxTop, topCollision
+
+// page geometry
+var styles, height, width, aspectRatio, vertCrop, bottomCrop, topCrop, trueHeight
 var trueAspectRatio = 1.5
-var boxTopPercent = 0.15
 var bottomCropFactor = 0.4
+var topCropFactor = 1 - bottomCropFactor
+
+// resize contact link
+var boxHeight, boxTop, topCollision
+var boxTopPercent = 0.15
 var contactLinkHeight = 40 // px
+
+// resize title
+var titleHeight, fontSize
+var referenceHeight = 820 // px
+var titleMaxTopOffset = 560 // px
+var titleMinTopOffset = 250 // px
+var trueTitleHeight = 310 // px
 
 responsiveLayout()
 window.onresize = responsiveLayout
-
-setTimeout(function() {
-	header.style.opacity = 1
-},400)		
+setTimeout(revealTitle,400)		
 
 
 function responsiveLayout() {
@@ -25,6 +36,8 @@ function responsiveLayout() {
 	horzCrop = aspectRatio < trueAspectRatio
 	
 	trueHeight = vertCrop ? width / trueAspectRatio : height
+	
+	// resize contact link
 	bottomCrop = vertCrop ? (trueHeight - height) * bottomCropFactor : 0
 	boxTop = trueHeight * boxTopPercent - bottomCrop
 	if (boxTop < contactLinkHeight) {
@@ -37,6 +50,27 @@ function responsiveLayout() {
 		body.style.fontSize = null
 
 		contactLink.style.bottom = (boxTop - contactLinkHeight ) / 2 + "px"
-
 	}
+
+	// resize title
+	var imageScale = trueHeight/referenceHeight
+	if (vertCrop) {
+		topCrop = (trueHeight - height) * topCropFactor
+		titleSpace = titleMaxTopOffset * imageScale - topCrop 
+		titleHeight = trueTitleHeight * imageScale - topCrop 
+		titleOffset = titleSpace - titleHeight
+	} else {
+		titleHeight = trueTitleHeight * imageScale
+		titleSpace = titleMaxTopOffset * imageScale
+		titleOffset = titleSpace - titleHeight
+	}
+	fontSize = (1/35) * titleHeight // yay for magic numbers
+	titleWidth =  80.4 * fontSize
+	fontSize = (titleWidth >= width) ? width / 80.4 : fontSize
+	body.style.fontSize = fontSize + "px"
+	header.style.top = titleOffset
+}
+
+function revealTitle() {
+	header.style.opacity = 1
 }
