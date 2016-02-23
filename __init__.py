1086c09028c6ce98709aaec9b0a66102198e67e0
MattSegal <mattdsegal@gmail.com>
2016-02-23 15:22:36 +1100

    changed sqlite to mysql database 2

--- __init__.py
+++ __init__.py
@@ -15,29 +15,29 @@ def contact():
 
 @app.route('/email/',methods=['POST'])
 def post_email():
-	email = request.get_data()
-	if email_db.is_valid_email(email) and email_db.post_email(email,test=True):
-		resp = Response(response='Successfully posted email',status = 200)
-	else:
-		resp = Response(response='Failed to post email',status = 500)
-	return resp
+    email = request.get_data()
+    if email_db.is_valid_email(email) and email_db.post_email(email,test=True):
+        resp = Response(response='Successfully posted email',status = 200)
+    else:
+        resp = Response(response='Failed to post email',status = 500)
+    return resp
 
 @app.route('/login/',methods=['GET'])
 def get_email():
-	return render_template('./email_login.html')
+    return render_template('./email_login.html')
 
 @app.route('/login/',methods=['POST'])
 def login():
-	password = request.form['password']
-	if is_valid_password(password):
-		emails = email_db.get_all_emails(test=True)
-		return render_template('./email_list.html',emails=emails)
-	else:
-		return render_template('./email_login.html')
+    password = request.form['password']
+    if is_valid_password(password):
+        emails = email_db.get_all_emails(test=True)
+        return render_template('./email_list.html',emails=emails)
+    else:
+        return render_template('./email_login.html')
 
 def is_valid_password(password):
-	PASSWORD_HASH = "1ee7760a3190c95641442f2be0ef7774e139fb1f"
-	return sha1(password).hexdigest() == PASSWORD_HASH
+    PASSWORD_HASH = "1ee7760a3190c95641442f2be0ef7774e139fb1f"
+    return sha1(password).hexdigest() == PASSWORD_HASH
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0',debug=True)
--- email_db.py
+++ email_db.py
@@ -1,6 +1,9 @@
-import sqlite3 as lite
+from mysql import connector as sql
+import gc # garbage collection
 import re as regex
 
+PROD_DB = "findingtheline"
+TEST_DB = "ftl_test"
 VALID_EMAIL_REGEX = '^(([^<>()\[\]\.,;:\s@"]+(\.[^<>()\[\]\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
 VALID_EMAILS = [
     "mattdsegal@gmail.com",
@@ -19,6 +22,23 @@ INVALID_EMAILS = [
     "gang99sta@bly",
     "example@email.com.",]
 
+def connect(test=True):
+    config = {
+        'user':'root',
+        'password':'chloe000',
+        'host':'localhost',
+        'database': TEST_DB if test else PROD_DB
+    }
+    con = sql.connect(**config)
+    cur = con.cursor()
+    return con, cur
+
+def disconnect(con,cur):
+    con.commit()
+    cur.close()
+    con.close()
+    gc.collect() # not sure if necessary, Sentdex guy thinks so
+
 def is_valid_email(email):
     if regex.match(VALID_EMAIL_REGEX, email):
         return True
@@ -26,29 +46,29 @@ def is_valid_email(email):
         return False
 
 def post_email(email, test=False):
-    db = 'test_db.sqlite' if test else 'database.sqlite'
-    con = lite.connect(db)
+    print email
+    (con,cur) = connect(test=test)
+    add_email = "INSERT INTO emails(email) VALUES(%s)"
+    cur.execute(add_email,(email,))
+    disconnect(con,cur)
+    return True
     try:
-        with con:
-            cur = con.cursor()
-            cur.execute("INSERT INTO emails(email) VALUES (?)",(email,))
-        return True
-    except sqlite3.OperationalError:
+        pass
+    except:
         return False
 
 def get_all_emails(test=False):
-    db = 'test_db.sqlite' if test else 'database.sqlite'
-    con = lite.connect(db)
     try:
-        with con:
-            cur = con.cursor()
-            cur.execute("SELECT email FROM emails")
-            emails_raw = cur.fetchall()
-            emails = []
-            for email in emails_raw:
-                emails.append(email[0])
-            return emails
-    except sqlite3.OperationalError:
+        (con,cur) = connect(test=test)
+        cur.execute("SELECT email FROM emails")
+        emails_raw = cur.fetchall()
+        emails = []
+        for email in emails_raw:
+            emails.append(email[0])
+        disconnect(con,cur)
+        return emails
+    except:
+        disconnect(con,cur)
         return [("Database error :(")]
 
 def seed_emails(test=True):
@@ -59,13 +79,34 @@ def seed_emails(test=True):
     assert len(emails) == len(VALID_EMAILS)
 
 def setup_email(test=True):
-    db = 'test_db.sqlite' if test else 'database.sqlite'
-    con = lite.connect(db)
-    with con:
-        cur = con.cursor()
-        cur.execute("DROP TABLE IF EXISTS emails")
-        create_users = "CREATE TABLE emails (id INTEGER PRIMARY KEY,email TEXT)"
-        cur.execute(create_users)
+    (con,cur) = connect(test=test)
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS emails")
+    create_users = "CREATE TABLE emails (id INT(11) AUTO_INCREMENT PRIMARY KEY,email VARCHAR(100))"
+    cur.execute(create_users)
+    cur.execute("DESCRIBE emails")
+    output = cur.fetchall()
+    print output
+    disconnect(con,cur)
+
+def test_post_email():
+    setup_email(test=True)
+    assert post_email("test@example.com",test=True)
+    (con,cur) = connect(test=True)
+    cur.execute("SELECT * FROM emails")
+    print str(cur.fetchone())
+    cur.execute("SELECT COUNT(email) FROM emails")
+    count = cur.fetchone()
+    assert count[0] == 1
+    disconnect(con,cur)
+
+def test_test_db():
+    (con,cur) = connect(test=True)
+    assert con.get_database() == u'ftl_test'
+
+def test_prod_db():
+    (con,cur) = connect(test=False)
+    assert con.get_database() == u'findingtheline'
 
 def test_is_valid_email():
     for email in VALID_EMAILS:
@@ -73,20 +114,10 @@ def test_is_valid_email():
     for email in INVALID_EMAILS:
          assert not is_valid_email(email) 
 
-def test_setup_email():
-    setup_email(test=True)
-    db = 'test_db.sqlite'
-    con = lite.connect(db)
-    with con:
-        cur = con.cursor()
-        cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='emails'")
-        table = cur.fetchone()
-        assert table[0] == "emails"
-
 def test_seed_emails():
     seed_emails()
 
 if __name__ == "__main__":
     pass
-    # setup_email(test=True)
+    setup_email(test=True)
     # setup_email(test=False)
