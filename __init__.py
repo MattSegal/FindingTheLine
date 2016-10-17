0d25b93bef29338737806c5a6fe928fa94495901
MattSegal <mattdsegal@gmail.com>
2016-10-17 22:59:32 +1100

    Restyle front page

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-
-from flask import * # fuck tha police
-import email_db
-from helper_functions import *
+from flask import * # Fix me
 from functools import wraps
+
+import email_db
+from membership import Security
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
@@ -40,12 +40,12 @@ def login():
     else:
         return render_template('./email_login.html')
 
-def login_required(f):
-    @wraps(f)
-    def wrapper (*args,**kwargs): # steal f's args
+def login_required(func):
+    @wraps(func)
+    def wrapper (*args,**kwargs): # steal func's args
         if security.is_logged_in(session):
-            # go on your merry way
-            return f(*args,**kwargs) 
+            # Go on your merry way
+            return func(*args,**kwargs) 
         else:
             return redirect(url_for('login'))
     return wrapper
@@ -53,7 +53,7 @@ def login_required(f):
 @app.route('/download/', methods=['GET'])
 @login_required
 def download():
-    emails = email_db.get_all_emails(test=False)
+    emails = email_db.get_all_emails(test=False) # This is yuck
     txt = ""
     for email in emails:
         txt += email+"\n"
@@ -62,4 +62,4 @@ def download():
     return response
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0',debug=False)
+    app.run(host= '0.0.0.0')
